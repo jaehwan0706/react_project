8754f9d6fb03b9f2e8da423f97e7f22180683301
jaehwan0706 <jaehwan0706@gmail.com>
2026-01-20 18:38:38 +0900

    ReactBook

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './react-project/sw/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
