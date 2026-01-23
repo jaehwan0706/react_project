a5a5b6c22a49059eef9f9edca006aa711184421c
jaehwan0706 <jaehwan0706@gmail.com>
2026-01-23 14:26:01 +0900

    병합3

--- src/react-project/sw/Header.js
+++ src/react-project/sw/Header.js
@@ -2,7 +2,12 @@ import logo from "./logo.png";
 
 function Header({ setMode }) {
   return (
-    <header className="header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '12px 16px' }}>
+    <header className="header" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', padding: '12px 16px', 
+      backgroundColor: '#549ee7', // 배경색을 연한 회색으로 변경 (원하는 색상 코드로 수정 가능)
+      borderBottom: '1px solid #eee', // 하단 구분선 추가
+
+
+      }}>
       <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
       <img
         src={logo}
